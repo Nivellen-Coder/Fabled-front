34fb5ec7b6f2565853481a2a7bed0d70e81e0161
Maxime Noël <maxime.noel19@gmail.com>
2023-12-18 17:16:21 +0100

    config for darkmode implementation into tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     "./src/**/*.{html,ts}",
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+    require('flowbite/plugin')
+  ],
 }
 
