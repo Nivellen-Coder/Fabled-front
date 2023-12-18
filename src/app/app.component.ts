5afa45c50cee936e7e3951b56cb7f70a7fef1629
Maxime Noël <maxime.noel19@gmail.com>
2023-12-18 17:34:37 +0100

    implementing logic for darkmode toggle button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2, RendererFactory2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ng-fabled-front';
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+    this.loadTheme();
+  }
+
+  // renderer: any;
+  title: any;
+  public theme: string = 'light';
+  private renderer: Renderer2;
+  private isDarkMode: boolean = false;
+
+  toggleDarkMode() {
+    this.isDarkMode = !this.isDarkMode;
+
+    if (this.isDarkMode) {
+      document.body.classList.add('dark');
+      document.body.classList.remove('light');
+      this.theme = "dark";
+    } else {
+      document.body.classList.add('light');
+      document.body.classList.remove('dark');
+      this.theme = "light";
+    }
+
+    this.toggleTheme();
+  }
+
+  isDarkModeActive(): boolean {
+    return this.isDarkMode;
+  }
+
+  constructor(rendererFactory: RendererFactory2) {
+    this.renderer = rendererFactory.createRenderer(null, null);
+    this.loadTheme();
+  }
+
+  toggleTheme() {
+    localStorage.setItem('theme', this.theme);
+  }
+
+  loadTheme() {
+    const storedTheme = localStorage.getItem('theme');
+    if (storedTheme) {
+      this.theme = storedTheme;
+      this.isDarkMode = this.theme === 'dark';
+      this.toggleDarkMode();
+    } else {
+      this.theme = 'light';
+      this.isDarkMode = false;
+      this.toggleDarkMode();
+    }
+  }
 }
