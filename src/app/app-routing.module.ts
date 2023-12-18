e6cec07a36b9a225dabe369e5b2f72ec0d4278e4
Maxime Noël <maxime.noel19@gmail.com>
2023-12-18 17:22:29 +0100

    adding route for UserRegisterComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserRegisterComponent } from "./components/user-register/user-register.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'user-register', component: UserRegisterComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
