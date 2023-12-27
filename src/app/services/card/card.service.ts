e332d3a6bdc3d809c481797bfa44f8a97a489be6
Maxime Noël <maxime.noel19@gmail.com>
2023-12-27 11:00:41 +0100

    card service creation + getAllCards method implementation

---
+++ src/app/services/card/card.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { environment } from 'src/environments/environment.development';
+import { Observable } from "rxjs";
+import { cardListModel } from "../../models/card/cardListModel";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CardService {
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Authorization': `Bearer ${environment.BEARER_TOKEN}`
+    })
+  };
+
+  constructor(private httpClient: HttpClient) { }
+
+  public getAllCards(page: number, pageSize: number): Observable<cardListModel>{
+    return this.httpClient.get<cardListModel>(environment.API_BASE_URL + "cards?time=" + Date.now() + "&hash=" + environment.API_HASH + "&per_page=" + pageSize + "&page=" + page, this.httpOptions);
+  }
+}
