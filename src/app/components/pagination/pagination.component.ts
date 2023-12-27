dc4ee3010ea247ffeadeeca269b0d76fbfcb959d
Maxime Noël <maxime.noel19@gmail.com>
2023-12-27 11:11:09 +0100

    ts file for pagination component implementation

--- src/app/components/pagination/pagination.component.ts
+++ src/app/components/pagination/pagination.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output, Input} from '@angular/core';
+
 
 @Component({
   selector: 'app-pagination',
@@ -6,5 +7,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./pagination.component.scss']
 })
 export class PaginationComponent {
+  @Input() currentPage: number = 1;
+  @Input() total: number = 1;
+  @Input() limit: number = 25;
+  @Input() pagesCount: number = 1;
+  @Input() name: string = "";
+  @Output() changePage = new EventEmitter<number>();
 
+  pages: number[] = [];
+  next: boolean = false;
+  previous: boolean = false;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.getPagesCount();
+  }
+
+  range(start: number, end: number): number[] {
+    return [...Array(end).keys()].map((el) => el + start);
+  }
+
+  onNextClick(): void {
+    if (this.currentPage < this.pagesCount) {
+      this.currentPage++;
+      this.changePage.emit(this.currentPage);
+      this.next = true;
+    }
+    this.next = false;
+  }
+
+  onPreviousClick(): void {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.changePage.emit(this.currentPage);
+      this.previous = true;
+    }
+    this.previous = false;
+  }
+
+  getPagesCount() {
+    this.pagesCount = Math.ceil(this.total / this.limit);
+    this.pages = this.range(1, this.pagesCount);
+  }
 }
