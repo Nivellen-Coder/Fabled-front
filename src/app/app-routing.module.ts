7dfff325e2e015f4e4750adcf0525c79caf1196f
Maxime Noël <maxime.noel19@gmail.com>
2023-12-27 14:11:58 +0100

    adding routes for card-list component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserRegisterComponent } from "./components/user-register/user-register.component";
+import { CardListComponent } from "./components/card-list/card-list.component";
 
 const routes: Routes = [
   { path: 'user-register', component: UserRegisterComponent },
+  { path: 'card-list', component: CardListComponent },
+  { path: '**', redirectTo: 'card-list' },
 ];
 
 @NgModule({
