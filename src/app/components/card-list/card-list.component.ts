1da16aa8450e1757df90c15c57c0b31430db7351
Maxime Noël <maxime.noel19@gmail.com>
2023-12-28 12:14:12 +0100

    adding some improvements to searching form

--- src/app/components/card-list/card-list.component.ts
+++ src/app/components/card-list/card-list.component.ts
@@ -19,24 +19,14 @@ export class CardListComponent implements OnInit {
   divList: any[] = new Array(48);
   total: number = 2000;
   isLoading: boolean = true;
+  isSearching: boolean = false;
 
   searchForm:FormGroup = new FormGroup({
     search:new FormControl('')
   })
 
   constructor(private cardService: CardService, private router: Router, private viewportScroller: ViewportScroller) {
-    this.searchForm.get('search')?.valueChanges.pipe(
-      debounceTime(1000),
-      distinctUntilChanged(),
-      switchMap((v) => this.cardService.getCardsByName(v, this.pageSize))
-    ).subscribe(
-      (result) => {
-        this.cards = result?.data;
-        this.currentPage = 1; // Réinitialise la page à 1 lors de la recherche
-        this.nbPage = result?.meta?.last_page || 1; // Met à jour le nombre de pages
-        this.total = result?.meta?.total || 0; // Met à jour le nombre total d'éléments
-      }
-    );
+    this.searchForCards(1);
   }
 
   ngOnInit(){
@@ -57,12 +47,42 @@ export class CardListComponent implements OnInit {
       this.total = cards.meta?.total;
       this.nbPage = cards.meta?.last_page;
       this.isLoading = false;
+      this.isSearching = false;
     });
+  }
+
+  public searchForCards(page: number): void {
+    this.searchForm.get('search')?.valueChanges.pipe(
+      debounceTime(1000),
+      distinctUntilChanged(),
+      switchMap((v) => this.cardService.getCardsByName(v, this.pageSize, page))
+    ).subscribe(
+      (result) => {
+        this.cards = result?.data;
+        this.nbPage = result?.meta?.last_page || 1; // Met à jour le nombre de pages
+        this.total = result?.meta?.total || 0; // Met à jour le nombre total d'éléments
+        this.isSearching = true;
+        this.currentPage = 1;
+      }
+    );
   }
 
   changePage(page: number): void {
     this.currentPage = page;
-    this.loadCards();
+
+    if(this.isSearching) {
+      this.cardService.getCardsByName(this.searchForm.get('search')?.value, this.pageSize, this.currentPage).subscribe((cards: cardListModel) => {
+        if(!this.cards){
+          this.isLoading = true;
+        }
+        this.cards = cards.data;
+        this.isLoading = false;
+        this.isSearching = false;
+      });
+    } else {
+      this.loadCards();
+    }
+
     this.viewportScroller.scrollToPosition([0, 0]);
   }
 }
