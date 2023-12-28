17cb274ffc4e9c0db74cdce38715c8c77ea5c2c9
Maxime Noël <maxime.noel19@gmail.com>
2023-12-28 10:56:33 +0100

    set up route for card-detail component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserRegisterComponent } from "./components/user-register/user-register.component";
 import { CardListComponent } from "./components/card-list/card-list.component";
+import { CardDetailComponent } from "./components/card-detail/card-detail.component";
 
 const routes: Routes = [
   { path: 'user-register', component: UserRegisterComponent },
   { path: 'card-list', component: CardListComponent },
+  { path: 'card-detail/:id', component: CardDetailComponent, pathMatch: 'full'},
   { path: '**', redirectTo: 'card-list' },
 ];
 
