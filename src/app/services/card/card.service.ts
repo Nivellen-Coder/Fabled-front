d083a5c15b46f4750b15a169160a15607ca2e6a4
Maxime Noël <maxime.noel19@gmail.com>
2023-12-28 10:49:23 +0100

    getCardById method implementation

--- src/app/services/card/card.service.ts
+++ src/app/services/card/card.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { environment } from 'src/environments/environment.development';
 import { Observable } from "rxjs";
 import { cardListModel } from "../../models/card/cardListModel";
+import { CardDetailModel } from "../../models/card/cardDetailModel";
 
 @Injectable({
   providedIn: 'root'
@@ -18,5 +19,9 @@ export class CardService {
 
   public getAllCards(page: number, pageSize: number): Observable<cardListModel>{
     return this.httpClient.get<cardListModel>(environment.API_BASE_URL + "cards?time=" + Date.now() + "&hash=" + environment.API_HASH + "&per_page=" + pageSize + "&page=" + page, this.httpOptions);
+  }
+
+  public getCardById(id: string): Observable<CardDetailModel>{
+    return this.httpClient.get<CardDetailModel>(environment.API_BASE_URL + 'cards/' + id + "?time=" + Date.now() + "&hash=" + environment.API_HASH, this.httpOptions)
   }
 }
