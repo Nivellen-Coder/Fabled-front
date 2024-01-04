56d5ce2d61ee590c5d73282b39485fe1534c3972
Maxime Noël <maxime.noel19@gmail.com>
2024-01-04 15:15:57 +0100

    adding new routes for new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { UserRegisterComponent } from "./components/user-register/user-register.component";
 import { CardListComponent } from "./components/card-list/card-list.component";
 import { CardDetailComponent } from "./components/card-detail/card-detail.component";
+import { LoginComponent } from "./components/login/login.component";
+import {UserProfileComponent} from "./components/user-profile/user-profile.component";
+import {AuthGuard} from "./guards/auth.guard";
 
 const routes: Routes = [
   { path: 'user-register', component: UserRegisterComponent },
   { path: 'card-list', component: CardListComponent },
   { path: 'card-detail/:id', component: CardDetailComponent, pathMatch: 'full'},
+  { path: 'user-login', component: LoginComponent},
+  { path: 'user-profile', component: UserProfileComponent, canActivate: [AuthGuard] },
   { path: '**', redirectTo: 'card-list' },
 ];
 
