0c35615b9f5122fbf228a0705c3f892dc5490bba
Maxime Noël <maxime.noel19@gmail.com>
2024-01-04 15:13:46 +0100

    user service improvements

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -2,24 +2,40 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse, HttpHeaders } from "@angular/common/http";
 import { catchError, Observable, throwError } from "rxjs";
 import { UserCreateModel } from "../../models/user/userCreateModel";
+import {UserInfosModel} from "../../models/user/userInfosModel";
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
+  // @ts-ignore
+  bearerToken = JSON.parse(localStorage.getItem('jwt'));
+
   private apiURL = 'http://127.0.0.1:8000/api/user';
-  constructor( private httpClient: HttpClient ) { }
+
+  constructor( private httpClient: HttpClient) { }
 
   httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
+      'Authorization': `Bearer ${this.bearerToken.token}`
     })
   }
+
+
 
   public userCreate(user : UserCreateModel): Observable<UserCreateModel> {
     console.log(JSON.stringify(user));
     return this.httpClient.post<UserCreateModel>(this.apiURL + '/create', JSON.stringify(user), this.httpOptions )
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  public userProfile(username: string): Observable<UserInfosModel> {
+    return this.httpClient.get<UserInfosModel>(`${this.apiURL}/profile/${username}`, this.httpOptions)
       .pipe(
         catchError(this.handleError)
       );
